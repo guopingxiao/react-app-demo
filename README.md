c9cbe2d27a134c059f41b0c9be86e427f954a234
xiaoguoping <xiaoguoping@meituan.com>
2018-11-04 11:29:42 +0800

    add antd

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ React TodoList 学习React的基本用法
 
 - react-simple-use
 - react-redux-use
+
+## 使用antd 来重新编写todolist
+
+因为目前antd 在后端项目中，用的还是比较多，所以可以直接拿过来用，直接看官方文档就能直接上手了。
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,105 +1,31 @@
-import React, { Component, Fragment } from 'react';
-import axios from 'axios'
-import TodoItem from './TodoItem'
-import Toggle from './Toggle'
-// import Test from './Test'
+import React, { Component } from 'react'
 
-class TodoList extends Component {
+import { Input, Button, List } from 'antd'
+import "antd/dist/antd.css"
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputValue: '',
-      todolist:[]
-    }
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.addBntTap = this.addBntTap.bind(this)
-    this.deleteItem = this.deleteItem.bind(this)
-  }
-  /* // 在组件即将挂载到页面时触发
-  componentWillMount() { 
-    console.log('componentWillMount')
-  }
 
-  // 非首次渲染后，组件是否要更新呀
-  shouldComponentUpdate() {
-    console.log('shouldComponentUpdate')
-    return true
-  }
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+]
+class TodoList extends Component { 
 
-  // 组件即将被更新
-  componentWillUpdate() { 
-    console.log('componentWillUpdate')
-  } */
-
-  render() {
-    console.log('parent render')
+  render() { 
     return (
-      <Fragment>
-        <Toggle></Toggle>
-        <input ref={(input) => { this.input = input}} value={this.state.inputValue} onChange={this.handleInputChange}></input>
-        <button onClick={this.addBntTap}>提交</button>
-        <ul ref={(ul) => { this.ul = ul}}>
-          {
-            this.getTodoItems()
-          }
-        </ul>
-        {/* <Test content={this.state.inputValue}></Test>*/}
-      </Fragment>
-    );
-  }
-  // 组件被挂载到页面上后，执行
-  componentDidMount() { 
-    // console.log('componentDidMount')
-    axios.get('/api/todolist')
-      .then((res) => { 
-        if (res.data) { 
-          this.setState(() => ({
-            todolist:[...res.data]
-          }))
-        }
-      })
-      .catch(err => { 
-        console.log(err)
-      })
-  }
-
-  getTodoItems() { 
-    let todolist = this.state.todolist
-      return todolist.map((item, index) => { 
-        return <TodoItem key={index} content={item} index={index} deleteItem={this.deleteItem}/>
-      })
-  }
-  handleInputChange(e) { 
-    // let inputValue = e.target.value
-    let inputValue = this.input.value
-    this.setState(() => ({
-      inputValue
-    }))
-  }
-
-  addBntTap() { 
-    if (!this.state.inputValue) { 
-      return
-    }
-    // setState是异步的，所以提供回调用于后续的异步之后的任务。
-    this.setState((preState) => (
-      {
-        todolist:[...preState.todolist, preState.inputValue],
-        inputValue: ''
-      }), () => { 
-        console.log(this.ul.querySelectorAll('li').length)
-      })
-  }
-
-  deleteItem(index) {
-    
-    this.setState((preState) => { 
-      let todolist = [...preState.todolist]
-      todolist.splice(index, 1)
-      return {todolist}
-    })
+      <div style={{margin:'10px 0 0 10px'}}>
+        <Input placeholder="todo info" style={{width:'300px'}}/>
+        <Button type="primary" style={{marginLeft:'10px'}}>提交</Button>
+        <List style={{ marginTop: '10px',width:'300px'}}
+          bordered
+          dataSource={data}
+          renderItem={item => (<List.Item>{item}</List.Item>)}
+        />
+      </div>
+    )
   }
 }
 
-export default TodoList;
+export default TodoList
